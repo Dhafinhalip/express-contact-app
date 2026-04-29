19d7292a752f8396f429076a4e9121ee65df9832
Dhafin Halip <dhafinhalip12@gmail.com>
2026-04-29 21:45:21 +0800

    feat: fix path

--- utils/filecheck.js
+++ utils/filecheck.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
+const fs = require("node:fs");
 
 //Folder and JSON File Checking
 function checkFolder() {
-  const folderPath = "../data";
+  const folderPath = "./data";
   if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath);
   }
 }
 
 function checkJsonFile() {
-  const jsonFilePath = "../data/contacts.json";
+  const jsonFilePath = "./data/contacts.json";
   if (!fs.existsSync(jsonFilePath)) {
     fs.writeFileSync(jsonFilePath, "[]", "utf-8");
   }
