238d289f41285971efee83e974a39eefbc18a2ea
Dhafin Halip <dhafinhalip12@gmail.com>
2026-04-29 21:47:13 +0800

    feat: update route

--- routes/routes.js
+++ routes/routes.js
@@ -7,14 +7,10 @@ const contactController = require("../controller/contactController");
 //1. route menampilkan halaman index
 router.get("/", contactController.index);
 
-//2. route menampilkan halaman form tambah kontak
-router.get("/add", contactController.addForm);
-
-//3. route untuk memproses data dari form
+//2. route untuk memproses data dari form
 router.post("/", contactController.store);
-javascript;
 
-//4. route untuk menghapus kontak
+//3. route untuk menghapus kontak
 router.post("/delete", contactController.destroy);
 
 module.exports = router;
